ee82c167c261b585e0b8771ea9cf43767e141b61
muratpala007 <muratpala007@gmail.com>
2022-04-19 22:31:30 +0300

    first commit

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+    var renk1 = "red";
+    var renk2 = "yellow";
+    var renk3 = "purple"
+
+    var renkler = new Array("red","yellow","purple");
+
+    renkler[1] = "pink";
+    document.write(renkler[0] + "<br>");
+    document.write(renkler[1] + "<br>");
+    document.write(renkler[2] + "<br>");
